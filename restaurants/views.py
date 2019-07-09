91563b4dcb23d799e5df9d883b879bce0a01206c
leyvai <bu50.88@gmail.com>
2019-07-09 20:37:06 +0300

    added task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    	"my_list" : [
+			{
+			"restaurant_name": "Cane's Chicken",
+			"food_type" : "Chicken",
+			},
+			{
+			"restaurant_name": "Pizza Hut",
+			"food_type" : "Pizza",
+			},
+			{
+			"restaurant_name" : "Johnny Rockets",
+			"food_type" : "Burgers",
+			},
+    	]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +28,11 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    	"my_object" :
+    		{
+    		"restaurant_name" : "Burger Boutique",
+    		"food_type" : "Burgers",
+    		},
+ 
     }
     return render(request, 'detail.html', context)
